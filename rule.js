d050090f1f75e28db0be94a7ef6e6316a8a28623
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2023-08-13 02:31:23 +0900

    Ignore no name for node

--- rule.js
+++ rule.js
@@ -10,7 +10,7 @@ module.exports = {
         create: function (context, ruleConfig) {
             return {
                 "node": function (node) {
-                    if (!node.config.name.match(/^[ -~]+$/)) {
+                    if (!node.config.name.match(/^[ -~]*$/)) {
                         context.report({
                             location: [node.id],
                             message: "ノード名は英数字、または記号である必要があります"
